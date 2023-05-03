e29e0e8ebcfa4fc8770a48f11c10890a7517b47d
jack <jackayron5@gmail.com>
2023-05-03 12:48:46 +0530

    done

--- index.js
+++ index.js
@@ -34,22 +34,7 @@ document.getElementById("resume-link-1").addEventListener("click", () => {
     sidemenu.style.right = "-200px";
   };
   
-  const scriptURL =
-    "https://script.google.com/macros/s/AKfycbx82hI_piwkiQD_at1DeSyXlBLq53Twggg1suj3j8zfUQryozddn4zZHOQb1bYPbQfs/exec";
-  const form = document.forms["submit-to-google-sheet"];
-  const msg = document.getElementById("msg");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    fetch(scriptURL, { method: "POST", body: new FormData(form) })
-      .then((response) => {
-        msg.innerHTML = "Message Sent Successfully";
-        setTimeout(() => {
-          msg.innerHTML = "";
-        }, 1000);
-        form.reset();
-      })
-      .catch((error) => console.error("Error!", error.message));
-  });
+
   
   window.addEventListener("scroll", function () {
     let nav = this.document.querySelector("nav");
